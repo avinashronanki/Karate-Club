5d1a2b0b5f46ae909c0d471be92cb952bcdfa398
avinashronanki <ronanki.avinash@gmail.com>
2020-07-07 00:29:57 +0200

    commit

---
+++ example.py
@@ -0,0 +1,19 @@
+# import networkx as nx
+# from karateclub import DeepWalk
+#
+# g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
+#
+# model = DeepWalk()
+# model.fit(g)
+# embedding = model.get_embedding()
+# print(embedding)
+
+import networkx as nx
+from karateclub.node_embedding.neighbourhood import Walklets
+
+g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
+
+model = Walklets()
+model.fit(g)
+embedding = model.get_embedding()
+print(embedding)
